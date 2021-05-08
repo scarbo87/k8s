a9fc06de48e4418a024be6aea2f1dff4b16c8b20
scarbo87 <scarbo1987@gmail.com>
2021-05-08 16:03:57 +0300

    added headers sorting

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"net/http/pprof"
 	"os"
 	"os/signal"
+	"sort"
 	"strings"
 	"sync"
 	"syscall"
@@ -19,18 +20,19 @@ import (
 )
 
 var (
-	apiAddress        string
+	mainApiAddress    string
 	monitoringAddress string
 	launchDelay       = time.Second * 3
 	shutdownDelay     = time.Second * 3
 	isReady           = atomic.NewBool(false)
-	err               error
 )
 
 func init() {
-	apiAddress = os.Getenv("API_ADDRESS")
-	if apiAddress == "" {
-		apiAddress = ":8000"
+	var err error
+
+	mainApiAddress = os.Getenv("MAIN_API_ADDRESS")
+	if mainApiAddress == "" {
+		mainApiAddress = ":8000"
 	}
 
 	monitoringAddress = os.Getenv("MONITORING_ADDRESS")
@@ -64,20 +66,22 @@ func main() {
 		cancel()
 	}()
 
-	log.Printf("Run monitoring server by address %s\n", monitoringAddress)
+	log.Printf("Run the monitoring server by address %s\n", monitoringAddress)
 	monitoringServer := runServer(monitoringAddress, getMonitoringHandler())
 
-	log.Printf("Emulate launch application, delay %s\n", launchDelay)
-	time.Sleep(launchDelay)
+	if launchDelay > 0 {
+		log.Printf("Emulate launch the main application, delay %s\n", launchDelay)
+		time.Sleep(launchDelay)
+	}
 
-	log.Printf("Run api server by address %s\n", apiAddress)
-	apiServer := runServer(apiAddress, &apiHandler{})
+	log.Printf("Run the main API server by address %s\n", mainApiAddress)
+	apiServer := runServer(mainApiAddress, &apiHandler{})
 	isReady.Store(true)
 
 	<-ctx.Done()
 	isReady.Store(false)
-	log.Printf("Shutdown application, delay %s\n", shutdownDelay)
 
+	log.Printf("Shutdown the application, delay %s\n", shutdownDelay)
 	ctx, cancel = context.WithTimeout(ctx, shutdownDelay)
 	defer cancel()
 
@@ -120,14 +124,26 @@ type apiHandler struct {
 }
 
 func (h *apiHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	type header struct {
+		key    string
+		values []string
+	}
+	headers := make([]header, 0, len(r.Header))
+	for k, v := range r.Header {
+		headers = append(headers, header{key: k, values: v})
+	}
+	sort.Slice(headers, func(i, j int) bool {
+		return headers[i].key < headers[j].key
+	})
+
 	builder := &strings.Builder{}
 	_, _ = fmt.Fprintf(builder, "%s %s %s\n", r.Method, r.URL, r.Proto)
-	_, _ = fmt.Fprintln(builder, "Headers:")
-	for k, v := range r.Header {
-		_, _ = fmt.Fprintf(builder, "\t%q = %q\n", k, v)
-	}
 	_, _ = fmt.Fprintf(builder, "Host = %q\n", r.Host)
 	_, _ = fmt.Fprintf(builder, "RemoteAddr = %q\n", r.RemoteAddr)
+	_, _ = fmt.Fprintln(builder, "Headers:")
+	for _, h := range headers {
+		_, _ = fmt.Fprintf(builder, "\t%q = %q\n", h.key, h.values)
+	}
 
 	_, _ = rw.Write([]byte(builder.String()))
 }
@@ -141,13 +157,12 @@ func runServer(address string, handler http.Handler) *http.Server {
 		wg.Done()
 		err := server.ListenAndServe()
 		if err != nil {
-			if errors.Is(err, http.ErrServerClosed) {
-				log.Printf("The server by address %s was closed\n", address)
-				return
+			if !errors.Is(err, http.ErrServerClosed) {
+				log.Fatalf("An error %v occurred for the server by address %s\n", err, address)
 			}
-			log.Fatalf("An error %v occurred while running server by address %s", err, address)
 		}
 	}()
 	wg.Wait()
+
 	return server
 }
